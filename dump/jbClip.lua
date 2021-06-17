c69fd8f9f29eee2ea7e1f9c3c70365ef586112a4
iamtryingtofindname <70244612+iamtryingtofindname@users.noreply.github.com>
2021-06-16 20:51:56 -0400

    Update jbClip.lua

--- dump/jbClip.lua
+++ dump/jbClip.lua
@@ -4,6 +4,8 @@ local forward = 8
 local up = 0
 local side = 0
 
+local key = "T"
+
 pcall(function()
     shared.e:Disconnect()
     shared.j:Disconnect();
@@ -13,8 +15,8 @@ shared.j = game.Players.LocalPlayer.CharacterAdded:Connect(function(c)
     root = c.PrimaryPart
 end)
 
-shared.e = game:GetService("UserInputService").InputBegan:Connect(function(input)
-    if input.UserInputType == Enum.UserInputType.Keyboard and input.KeyCode == Enum.KeyCode.F8 then
+shared.e = game:GetService("UserInputService").InputBegan:Connect(function(input,gpe)
+    if (not gpe) and input.UserInputType == Enum.UserInputType.Keyboard and input.KeyCode == Enum.KeyCode[key] then
         root.CFrame = root.CFrame * CFrame.new(side,up,-forward)
     end
 end)
