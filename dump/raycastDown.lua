ba7c585509e2467c7053a5c1467426b1e9bf3252
iamtryingtofindname <70244612+iamtryingtofindname@users.noreply.github.com>
2021-07-04 13:31:33 -0400

    Update raycastDown.lua

--- dump/raycastDown.lua
+++ dump/raycastDown.lua
@@ -4,14 +4,28 @@ local raycastParams = RaycastParams.new()
 raycastParams.FilterDescendantsInstances = {character}
 raycastParams.FilterType = Enum.RaycastFilterType.Blacklist
 
-local ray = workspace:Raycast(character.PrimaryPart.Position, Vector3.new(0,-100,0),raycastParams)
+local function doRaycast()
+	local ray = workspace:Raycast(character.PrimaryPart.Position, Vector3.new(0,-100,0),raycastParams)
 
-if ray and ray.Instance then
-    local hit = ray.Instance
-    print("Instance found!")
-    print(hit.Name.." ("..hit:GetFullName()..")")
-else
-    print("No instance found!")
+	if ray and ray.Instance then
+		local hit = ray.Instance
+		print("Instance found!")
+		print(hit.Name.." ("..hit:GetFullName()..")")
+	else
+		print("No instance found!")
+	end
+
+	print("---------------------------")
 end
 
-print("---------------------------")
+pcall(function()
+	shared.z:Disconnect()
+end)
+
+local key = "N"
+
+shared.z = game:GetService("UserInputService").InputBegan:Connect(function(input,gpe)
+	if not gpe and input.UserInputType == Enum.UserInputType.Keyboard and input.KeyCode == Enum.KeyCode[key] then
+		doRaycast()
+	end
+end)
