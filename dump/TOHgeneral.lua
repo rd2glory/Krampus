ac2f0fb5b59590ea1aff169d98aee530055884e0
iamtryingtofindname <70244612+iamtryingtofindname@users.noreply.github.com>
2021-07-14 21:50:10 -0400

    Create TOHgeneral.lua

---
+++ dump/TOHgeneral.lua
@@ -0,0 +1,2 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/iamtryingtofindname/Krampus/main/dump/TOHantiCheatBypass.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/iamtryingtofindname/Krampus/main/dump/TOHnewKillPartRemover.lua"))()
