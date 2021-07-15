162ee9dd01267ce44b093099f6e8b1ac3878d9ca
iamtryingtofindname <70244612+iamtryingtofindname@users.noreply.github.com>
2021-07-14 21:54:02 -0400

    Update TOHnewKillPartRemover.lua

--- dump/TOHnewKillPartRemover.lua
+++ dump/TOHnewKillPartRemover.lua
@@ -1,5 +1,10 @@
-for i,v in pairs(game:GetService("Workspace").tower:GetDescendants()) do
-    if v:IsA("BoolValue") and v.Name == "kills" then
-        v.Parent:Destroy()
+local function update()
+    for i,v in pairs(game:GetService("Workspace").tower:GetDescendants()) do
+        if v:IsA("BoolValue") and v.Name == "kills" then
+            v.Parent:Destroy()
+        end
     end
 end
+
+workspace.ChildAdded:Connect(update)
+workspace.ChildRemoved:Connect(update)
